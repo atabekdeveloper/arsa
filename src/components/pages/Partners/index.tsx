f6dc5aed71c86d2f2f472f44f89354e7b83b210d
koujidev <atabek122002@gmail.com>
2024-10-15 16:14:51 +0500

    Оптимизировал блок Partners

--- src/components/pages/Partners/index.tsx
+++ src/components/pages/Partners/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
 
 import p1 from 'src/assets/partners/1.svg';
 import p2 from 'src/assets/partners/2.png';
@@ -9,10 +7,8 @@ import p3 from 'src/assets/partners/3.png';
 import p4 from 'src/assets/partners/4.png';
 import p5 from 'src/assets/partners/5.png';
 import p6 from 'src/assets/partners/6.png';
-import p7 from 'src/assets/partners/8.png';
-import p8 from 'src/assets/partners/9.png';
-import p9 from 'src/assets/partners/10.png';
-import p10 from 'src/assets/partners/11.png';
+import p7 from 'src/assets/partners/7.png';
+import p8 from 'src/assets/partners/8.png';
 
 const partners = [
   { src: p1, alt: 'General Motors' },
@@ -22,9 +18,7 @@ const partners = [
   { src: p5, alt: 'AVTOYNA' },
   { src: p6, alt: 'AUTOPODBOR' },
   { src: p7, alt: 'Prime Auto LTD' },
-  // { src: p8, alt: 'CAR Wings' },
-  { src: p9, alt: 'HANGLASS' },
-  // { src: p10, alt: 'ASL PROF' },
+  { src: p8, alt: 'HANGLASS' },
 ];
 
 export const Partners: React.FC = () => {
@@ -42,23 +36,6 @@ export const Partners: React.FC = () => {
           />
         ))}
       </div>
-      {/* <Swiper
-        className="block py-4 mySwiper md:hidden"
-        rewind
-        modules={[Autoplay]}
-        slidesPerView={3}
-        spaceBetween={30}
-        autoplay={{
-          delay: 1500,
-          disableOnInteraction: false,
-        }}
-      >
-        {partners.map((partner, index) => (
-          <SwiperSlide key={index}>
-            <img key={index} className="max-w-[100px]" src={partner.src} alt={partner.alt} />
-          </SwiperSlide>
-        ))}
-      </Swiper> */}
     </div>
   );
 };
