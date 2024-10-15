3d6fa474438279e9d32fa8e6d87d3ff8f4fddb8d
koujidev <atabek122002@gmail.com>
2024-10-15 18:00:37 +0500

    Оптимизировал картинки partners

--- src/components/pages/Partners/index.tsx
+++ src/components/pages/Partners/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import p1 from 'src/assets/partners/1.svg';
+import p1 from 'src/assets/partners/1.png';
 import p2 from 'src/assets/partners/2.png';
 import p3 from 'src/assets/partners/3.png';
 import p4 from 'src/assets/partners/4.png';
@@ -11,14 +11,14 @@ import p7 from 'src/assets/partners/7.png';
 import p8 from 'src/assets/partners/8.png';
 
 const partners = [
-  { src: p1, alt: 'General Motors' },
-  { src: p2, alt: 'Artel' },
-  { src: p3, alt: 'AutoGlass' },
-  { src: p4, alt: 'AVDN' },
-  { src: p5, alt: 'AVTOYNA' },
-  { src: p6, alt: 'AUTOPODBOR' },
-  { src: p7, alt: 'Prime Auto LTD' },
-  { src: p8, alt: 'HANGLASS' },
+  { src: p2 },
+  { src: p1 },
+  { src: p3 },
+  { src: p4 },
+  { src: p5 },
+  { src: p6 },
+  { src: p7 },
+  { src: p8 },
 ];
 
 export const Partners: React.FC = () => {
@@ -26,13 +26,13 @@ export const Partners: React.FC = () => {
   return (
     <div id="partners">
       <h2 className="title">{t('navPartners')}</h2>
-      <div className="flex flex-wrap items-center justify-center gap-16 mb-20 -mt-5 md:justify-around">
+      <div className="flex flex-wrap items-center justify-center gap-16 my-16 md:justify-around">
         {partners.map((partner, index) => (
           <img
             key={index}
             className="max-w-[200px] object-contain"
             src={partner.src}
-            alt={partner.alt}
+            alt={partner.src}
           />
         ))}
       </div>
