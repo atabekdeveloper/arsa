5aee69c8203aea19af7e1cdd9a7269056bb58b6d
koujidev <atabek122002@gmail.com>
2024-10-16 15:04:33 +0500

    Изменил иконки Experience

--- src/components/pages/Experience/index.tsx
+++ src/components/pages/Experience/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { CountUp } from 'use-count-up';
-import { FaBoxes, FaUsers } from 'react-icons/fa';
 import { useInView } from 'react-intersection-observer';
 import { useTranslation } from 'react-i18next';
-import { HiOutlineCalendar } from 'react-icons/hi';
+import { IoCalendarClearOutline } from 'react-icons/io5';
+import { AiOutlineProduct } from 'react-icons/ai';
+import { IoPeopleOutline } from 'react-icons/io5';
 
 export const Experience: React.FC = () => {
   const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.5 });
@@ -13,7 +14,7 @@ export const Experience: React.FC = () => {
       <div className="grid max-w-full grid-cols-1 gap-12 mx-auto text-center md:grid-cols-3">
         <div className="p-8 transition-transform duration-300 transform bg-white rounded-lg shadow-lg hover:scale-105">
           <div className="flex justify-center mb-4 text-6xl text-primary">
-            <HiOutlineCalendar />
+            <IoCalendarClearOutline />
           </div>
           <h2 className="text-5xl font-bold text-gray-800">
             <CountUp isCounting={inView} start={1} end={8} duration={2} />+
@@ -22,7 +23,7 @@ export const Experience: React.FC = () => {
         </div>
         <div className="p-8 transition-transform duration-300 transform bg-white rounded-lg shadow-lg hover:scale-105">
           <div className="flex justify-center mb-4 text-6xl text-primary">
-            <FaBoxes />
+            <AiOutlineProduct />
           </div>
           <h2 className="text-5xl font-bold text-gray-800">
             <CountUp isCounting={inView} start={10} end={120} />+
@@ -31,7 +32,7 @@ export const Experience: React.FC = () => {
         </div>
         <div className="p-8 transition-transform duration-300 transform bg-white rounded-lg shadow-lg hover:scale-105">
           <div className="flex justify-center mb-4 text-6xl text-primary">
-            <FaUsers />
+            <IoPeopleOutline />
           </div>
           <h2 className="text-5xl font-bold text-gray-800">
             <CountUp isCounting={inView} start={9} end={30} />+
