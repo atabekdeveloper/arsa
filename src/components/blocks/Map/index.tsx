3608ebf6ab3aec8e2058040a61c599c626a7e23c
koujidev <atabek122002@gmail.com>
2024-10-29 14:08:22 +0500

    Изменил изображение

--- src/components/blocks/Map/index.tsx
+++ src/components/blocks/Map/index.tsx
@@ -3,5 +3,9 @@ import React from 'react';
 import map from 'src/assets/images/Uzbekistan.svg';
 
 export const Map: React.FC = () => {
-  return <img className="max-w-full" src={map} alt="Uzbekistan" />;
+  return (
+    <div className="container">
+      <img className="max-w-full" src={map} alt="Uzbekistan" />
+    </div>
+  );
 };
