b59d42ca65f29264f2f343a037372ecb53c4d1dc
koujidev <atabek122002@gmail.com>
2024-10-29 14:22:34 +0500

    Подправил неточность

--- src/components/blocks/Map/index.tsx
+++ src/components/blocks/Map/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import map from 'src/assets/images/Uzbekistan.svg';
 
 export const Map: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <div className="container relative mb-10" data-aos="zoom-in">
       <img className="max-w-full" src={map} alt="Uzbekistan" />
       <div className="static text-center lg:absolute lg:text-left bottom-24 left-24 text-primary">
-        <h3 className="text-xl font-bold md:text-2xl">Bizning korxona</h3>
-        <p className="text-base md:text-lg">O'zbekistonning barcha hududini qamrab olgan</p>
+        <h3 className="text-xl font-bold md:text-2xl">{t('mapTitle')}</h3>
+        <p className="text-base md:text-lg">{t('mapDesc')}</p>
       </div>
     </div>
   );
