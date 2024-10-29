c888bf927de84469a4ebc99e57e414e93aa38845
koujidev <atabek122002@gmail.com>
2024-10-29 16:10:37 +0500

    Подправил пиксели

--- src/components/blocks/Map/index.tsx
+++ src/components/blocks/Map/index.tsx
@@ -8,7 +8,7 @@ export const Map: React.FC = () => {
   return (
     <div className="container relative mb-10" data-aos="zoom-in">
       <img className="max-w-full" src={map} alt="Uzbekistan" />
-      <div className="static text-center lg:absolute lg:text-left bottom-24 left-24 text-primary">
+      <div className="static text-center lg:absolute lg:text-left bottom-44 left-24 text-primary">
         <h3 className="text-xl font-bold md:text-2xl">{t('mapTitle')}</h3>
         <p className="text-base md:text-lg">{t('mapDesc')}</p>
       </div>
